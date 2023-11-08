ad0cc92f58ba3deb07ce58e6e74bc5ce99071ab9
MarioBoss264 <77411035+MarioBoss264@users.noreply.github.com>
2023-11-08 20:07:31 +0100

    commit CFIL

--- src/hilos/CFil.java
+++ src/hilos/CFil.java
@@ -2,5 +2,34 @@ package hilos;
 
 
 public class CFil extends Thread {
+    private String nombre;
+    private int temporizacion;
 
+    public CFil(String nombre) {
+        this.nombre = nombre;
+        this.temporizacion = 0; // Valor predeterminado
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setTemporizacion(int temporizacion) {
+        this.temporizacion = temporizacion;
+    }
+
+    @Override
+    public void run() {
+        try {
+            System.out.println("Hilo " + nombre + " iniciado.");
+            // Realiza alguna tarea aquí
+
+            // Simula la temporización del hilo
+            Thread.sleep(temporizacion);
+
+            System.out.println("Hilo " + nombre + " terminado.");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }

acdd20e196fc9439707306c71e516d7189aa2759
Polito36 <polborbon@gmail.com>
2023-11-15 18:54:20 +0100

    a

--- src/hilos/CFilPrincipal.java
+++ src/hilos/CFilPrincipal.java
@@ -25,43 +25,64 @@ public class CFilPrincipal {
             System.out.println("\nMenú:");
             System.out.println("1. Modificar temporización para que los hijos terminen antes que el padre");
             System.out.println("2. Modificar temporización para que los hijos terminen después que el padre");
-            System.out.println("3. Salir");
+            System.out.println("3. Asegurar que el padre termine después de cualquier hijo");
+            System.out.println("4. Iniciar ejecución con temporizaciones crecientes");
+            System.out.println("5. Salir");
 
             System.out.println("Seleccione una opción: ");
             opcion = scanner.nextInt();
 
-            switch (opcion) {
-                case 1:
-                    for (int i = 0; i < numHijos; i++) {
-                        hijos[i] = new CFil("Hijo " + i);
-                        hijos[i].setTemporizacion(i * 500); // Modificar para que terminen antes
-                        hijos[i].start();
-                    }
-                    break;
-                case 2:
-                    for (int i = 0; i < numHijos; i++) {
-                        hijos[i] = new CFil("Hijo " + i);
-                        hijos[i].setTemporizacion(i * 2000); // Modificar para que terminen después
-                        hijos[i].start();
-                    }
-                    break;
-                case 3:
-                    break;
-                default:
-                    System.out.println("Opción no válida. Intente de nuevo.");
-            }
-
-            // Espera a que todos los hilos hijos terminen
-            for (int i = 0; i < numHijos; i++) {
-                try {
-                    hijos[i].join();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+            if (opcion == 1) {
+                for (int i = 0; i < numHijos; i++) {
+                    hijos[i] = new CFil("Hijo " + i);
+                    hijos[i].setTemporizacion(i * 500); // Modificar para que terminen antes
+                    hijos[i].start();
                 }
+            } else if (opcion == 2) {
+                for (int i = 0; i < numHijos; i++) {
+                    hijos[i] = new CFil("Hijo " + i);
+                    hijos[i].setTemporizacion(i * 2000); // Modificar para que terminen después
+                    hijos[i].start();
+                }
+            } else if (opcion == 3) {
+                for (int i = 0; i < numHijos; i++) {
+                    hijos[i] = new CFil("Hijo " + i);
+                    hijos[i].setTemporizacion(temporizacion);
+                    hijos[i].start();
+                    try {
+                        hijos[i].join(); // Espera a que cada hilo hijo termine antes de continuar con el padre
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } else if (opcion == 4) {
+                if (numHijos > 0) {
+                    for (int i = 0; i < numHijos; i++) {
+                        hijos[i] = new CFil("Hijo " + i);
+                        hijos[i].setTemporizacion(i * 1000); // Temporizaciones crecientes
+                        hijos[i].start();
+                    }
+
+                    // Espera a que todos los hilos hijos terminen
+                    for (int i = 0; i < numHijos; i++) {
+                        try {
+                            hijos[i].join();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                } else {
+                    System.out.println("Número de procesos hijos debe ser mayor que 0.");
+                }
+            } else if (opcion == 5) {
+                break;
+            } else {
+                System.out.println("Opción no válida. Intente de nuevo.");
             }
-        } while (opcion != 3);
+        } while (true);
 
         System.out.println("Proceso padre ha terminado.");
         scanner.close();
     }
 }
+

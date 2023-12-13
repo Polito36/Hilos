e624699769a47f530310f7f6c0a3a35abb36cbe7
Polito36 <polborbon@gmail.com>
2023-12-13 19:59:06 +0100

    3

---
+++ src/Procesos_Hilos3/Buscador.java
@@ -0,0 +1,37 @@
+package Procesos_Hilos3;
+
+import java.util.Random;
+import java.util.Scanner;
+
+class Buscador extends Thread {
+    private int[] vector;
+    private int inicio;
+    private int fin;
+    private int objetivo;
+    private boolean encontrado;
+
+    public Buscador(int[] vector, int inicio, int fin, int objetivo) {
+        this.vector = vector;
+        this.inicio = inicio;
+        this.fin = fin;
+        this.objetivo = objetivo;
+        this.encontrado = false;
+    }
+
+    @Override
+    public void run() {
+        for (int i = inicio; i <= fin; i++) {
+            if (vector[i] == objetivo) {
+                System.out.println("¡Valor encontrado en la posición " + i + " por el hilo " + Thread.currentThread().getId() + "!");
+                encontrado = true;
+                return;
+            }
+        }
+    }
+
+    public boolean isEncontrado() {
+        return encontrado;
+    }
+}
+
+

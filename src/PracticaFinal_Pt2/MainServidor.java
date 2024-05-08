0f16513b5964e344bde9387eac2b0b71f33ccaa3
Polito36 <polborbon@gmail.com>
2024-05-08 18:55:11 +0200

    a

--- src/PracticaFinal_Pt2/MainServidor.java
+++ src/PracticaFinal_Pt2/MainServidor.java
@@ -1,12 +1,17 @@
 package PracticaFinal_Pt2;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class MainServidor {
 
     public static void main(String[] args) {
-        int maxClientes = Integer.parseInt(args[0]);
-        Servidor servidor = new Servidor(maxClientes);
+    	
+    	Scanner s1 = new Scanner(System.in);
+    	System.out.println("Cuantos clientes quieres? ");
+    	int clientes = s1.nextInt();
+ 
+        Servidor servidor = new Servidor(clientes);
         servidor.runServer();
     }
 }
